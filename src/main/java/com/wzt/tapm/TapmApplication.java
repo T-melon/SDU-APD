0a5c65493d9133871c5fa0594bb6e2234b652037
unmatched-tsj <tianshujie@mail.sdu.edu.cn>
2021-11-24 19:32:49 +0800

    Empty frame.

---
+++ src/main/java/com/wzt/tapm/TapmApplication.java
@@ -0,0 +1,13 @@
+package com.wzt.tapm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TapmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wzt/tapm/TapmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzt.tapm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
