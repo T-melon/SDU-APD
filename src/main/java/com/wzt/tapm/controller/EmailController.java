6928f17797a6204ce0901b8eeccfad841b42b539
unmatched-tsj <tianshujie@mail.sdu.edu.cn>
2021-12-17 17:53:59 +0800

    Added email. Have Completed.

---
+++ src/main/java/com/wzt/tapm/controller/EmailController.java
@@ -0,0 +1,31 @@
+package com.wzt.tapm.controller;
+
+import com.wzt.tapm.entity.DemandBean;
+import com.wzt.tapm.service.EmailService;
+import com.wzt.tapm.service.EmailServiceImpl;
+import com.wzt.tapm.util.Result;
+import com.wzt.tapm.util.ResultCodeEnum;
+import com.wzt.tapm.util.annotation.UserLoginToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class EmailController {
+
+    @Resource
+    private final EmailServiceImpl emailServiceImpl;
+
+    public EmailController(EmailServiceImpl emailServiceImpl) {
+        this.emailServiceImpl = emailServiceImpl;
+    }
+    @UserLoginToken
+    @PostMapping("/demand/email")
+    public Result sendEmail(String to, String subject, String text) {
+        emailServiceImpl.sendSimpleMessage(to,subject,text);
+        Result result;
+        result = Result.getResult(ResultCodeEnum.SUCCESS);
+        return result;
+    }
+}
